a165116315d0549e1220d6302f5a83e4b1f2a259
Sofya <48826470+SofyaPim@users.noreply.github.com>
2020-06-22 13:49:00 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,57 +1,44 @@
-const requestUrl = 'https://github.com/SofyaPim/BootstrapProject1/data.html'
-const btn = $('.load-more');
-const loader = btn.find('span');
+"use strict";
+const requestUrl = "https://github.com/SofyaPim/BootstrapProject1/data.html";
+const btn = $(".load-more");
+const loader = btn.find("span");
 function sendRequest(method, url, body = null) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest;
-        xhr.open(method, url)
-        xhr.responseType = ""
-//to POST    
-//xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.onload = () => {
+    return new Promise( function (resolve, reject){
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = "";
+        xhr.onload = function()  {
             if (xhr.status >= 400) {
-                reject(xhr.response)
+                reject(xhr.response);
             } else {
-                resolve(xhr.response)
-            }
+                resolve(xhr.response);
+            };
         }
-        xhr.onerror = () => {
+        xhr.onerror = function(){
             reject(xhr.response);
 
         }
         xhr.send();
-        //to POST 
-        // xhr.send(JSON.stringify(body));
-
     })
 
 }
-sendRequest('GET', requestUrl)
+sendRequest("GET", requestUrl)
     .then(data => {
-        btn.on('click', function () {
+        btn.on("click", function () {
             
-            btn.attr('disabled', true);
-            loader.addClass('d-inline-block');
+            btn.attr("disabled", true);
+            loader.addClass("d-inline-block");
             setTimeout(function () {
-                                    loader.removeClass('d-inline-block');
-                                    btn.attr('disabled', false);
-                $('.after-posts').before(data)
+                                    loader.removeClass("d-inline-block");
+                                    btn.attr("disabled", false);
+                $(".after-posts").before(data)
                                 }, 2000);
         })
     })
 
     .catch(err => {
-         loader.removeClass('d-inline-block');
+         loader.removeClass("d-inline-block");
 
-           btn.attr('disabled', false);
-           
-                                
-                            
-    })
-//POST
- //sendRequest('POST')
-//  const body = { name: "Bob", age: 22}
-//sendRequest('GET', requestUrl, body)
-
-//const { get } = require("browser-sync").create();
+           btn.attr("disabled", false);
+      })
 
